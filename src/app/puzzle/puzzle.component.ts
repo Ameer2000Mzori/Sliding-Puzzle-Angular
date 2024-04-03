65656caed0041ee9e44c211423589088d0de6c9b
Ameer Ameen <ameerapex496@gmail.com>
2024-04-03 18:38:56 +0200

    generation of new component and connection to app component

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-puzzle',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './puzzle.component.html',
-  styleUrl: './puzzle.component.scss'
+  styleUrl: './puzzle.component.scss',
 })
-export class PuzzleComponent {
-
-}
+export class PuzzleComponent {}
