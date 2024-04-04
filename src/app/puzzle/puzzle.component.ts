625bb6cf94db09022278ae86f909451d41e3e8d6
Ameer Ameen <ameerapex496@gmail.com>
2024-04-04 18:56:21 +0200

    added date and used pipe for date and also added variable to store the amount of moves i did

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,11 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavUtilityComponent } from '../shared/nav-utility/nav-utility.component';
 
 @Component({
   selector: 'app-puzzle',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavUtilityComponent],
   templateUrl: './puzzle.component.html',
   styleUrl: './puzzle.component.scss',
 })
-export class PuzzleComponent {}
+export class PuzzleComponent implements OnInit {
+  board: number[][] = [];
+  emptyRow: number = 0;
+  emptyCol: number = 0;
+  gameOver: boolean = false;
+  move: number = 0;
+  time: number = 0;
+  date = new Date();
+
+  ngOnInit(): void {
+    this.shuffleBoard();
+    console.log(this.date);
+  }
+
+  shuffleBoard(): void {
+    let numbers = Array.from({ length: 15 }, (_, i) => i + 1);
+    this.board = [];
+    for (let row = 0; row < 4; row++) {
+      this.board[row] = [];
+      for (let col = 0; col < 4; col++) {
+        if (row === 3 && col === 3) {
+          this.board[row][col] = 0;
+          this.emptyRow = row;
+          this.emptyCol = col;
+        } else {
+          const index = Math.floor(Math.random() * numbers.length);
+          this.board[row][col] = numbers[index];
+          numbers.splice(index, 1);
+        }
+      }
+    }
+  }
+
+  moveTile(row: number, col: number): void {
+    if (
+      (Math.abs(row - this.emptyRow) === 1 && col === this.emptyCol) ||
+      (Math.abs(col - this.emptyCol) === 1 && row === this.emptyRow)
+    ) {
+      this.move += 1;
+      this.board[this.emptyRow][this.emptyCol] = this.board[row][col];
+      this.board[row][col] = 0;
+      this.emptyRow = row;
+      this.emptyCol = col;
+      console.log('moves', this.move);
+      this.checkWin();
+    }
+  }
+
+  checkWin(): void {
+    let count = 1;
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (
+          this.board[row][col] !== count &&
+          !(row === this.emptyRow && col === this.emptyCol)
+        ) {
+          return;
+        }
+        count++;
+      }
+    }
+    this.gameOver = true;
+  }
+}
